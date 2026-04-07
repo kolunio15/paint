8ae037b11f09246d62e3944569d12c870783259c
kolunio <110849867+kolunio15@users.noreply.github.com>
2026-04-07 11:35:46 +0200

    Add mock gallery

--- Paint/Pages/Index.cshtml.cs
+++ Paint/Pages/Index.cshtml.cs
@@ -1,10 +1,17 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Paint.Pages;
 
+public record Artwork(int Id, int CommentCount, int Points, string Title, string ImageUrl);
+
 public class IndexModel : PageModel
 {
+    public List<Artwork> Artworks { get; set; } = [
+        new(Id: 0, CommentCount: 2, Points: 15,  "Super kciuk!", "mock/kciuk.png"), 
+        new(Id: 1, CommentCount: 1, Points:  2,  "Mój dom",      "mock/domek.png"), 
+        new(Id: 2, CommentCount: 0, Points: -10, "Jeż",          "mock/jeż.png")
+    ];
+
     public void OnGet()
     {
     }
