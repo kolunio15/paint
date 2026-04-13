0bd74dbb1a9e6be0fb59dfc491799d1b5060a1e9
Jakub Celiński <k-celinski@wp.pl>
2026-04-13 22:32:50 +0200

    nowe arty

--- Paint/Pages/Index.cshtml.cs
+++ Paint/Pages/Index.cshtml.cs
@@ -9,8 +9,10 @@ public class IndexModel : PageModel
     public List<Artwork> Artworks { get; set; } = [
         new(Id: 0, CommentCount: 2, Points: 15,  "Super kciuk!", "mock/kciuk.png"), 
         new(Id: 1, CommentCount: 1, Points:  2,  "Mój dom",      "mock/domek.png"), 
-            new(Id: 2, CommentCount: 0, Points: -10, "Jeż",          "mock/jeż.png"),
-			new(Id: 3, CommentCount: 0, Points: 0, "OC DO NOT STEAL",          "mock/transistra.png")
+        new(Id: 2, CommentCount: 0, Points: -10, "Jeż",          "mock/jeż.png"),
+		new(Id: 3, CommentCount: 0, Points: 0, "OC DO NOT STEAL",          "mock/transistra.png"),
+        new(Id: 4, CommentCount: 0, Points: 0, "Micko Mouse",          "mock/micko_mouse.jpg"),
+        new(Id: 5, CommentCount: 0, Points: 0, "Garfield?",          "mock/kot_chyba.png")
     ];
 
     public void OnGet()
