571f53c9a2d89c87d80c5900a4ce990993319234
jfalba <01189127@pw.edu.pl>
2026-04-13 21:59:24 +0200

    Dodanie dodatkowych prac rysunkowych

--- Paint/Pages/Index.cshtml.cs
+++ Paint/Pages/Index.cshtml.cs
@@ -9,7 +9,8 @@ public class IndexModel : PageModel
     public List<Artwork> Artworks { get; set; } = [
         new(Id: 0, CommentCount: 2, Points: 15,  "Super kciuk!", "mock/kciuk.png"), 
         new(Id: 1, CommentCount: 1, Points:  2,  "Mój dom",      "mock/domek.png"), 
-            new(Id: 2, CommentCount: 0, Points: -10, "Jeż",          "mock/jeż.png")
+            new(Id: 2, CommentCount: 0, Points: -10, "Jeż",          "mock/jeż.png"),
+			new(Id: 3, CommentCount: 0, Points: 0, "OC DO NOT STEAL",          "mock/transistra.png")
     ];
 
     public void OnGet()
