884a71d8e3b9442f5a5ff78232c528804c6d0b59
PetraCore <102828971+PetraCore@users.noreply.github.com>
2026-06-02 14:41:35 +0200

    Fix connection issues

--- Paint/wwwroot/js/canvas.js
+++ Paint/wwwroot/js/canvas.js
@@ -14,7 +14,20 @@ const chatInput = document.getElementById("chatInput");
 const chatMessages = document.getElementById("chatMessages");
 const sizeInput = document.getElementById("size");
 
-const sessionClientId = crypto.randomUUID();
+function generateSessionId() {
+    // used on https or localhost connections
+    if (window.crypto && crypto.randomUUID) {
+        return crypto.randomUUID();
+    }
+    // fallback for http connections
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+const sessionClientId = generateSessionId();
 
 let painting = false;
 let currentMainColor = "#000000";
